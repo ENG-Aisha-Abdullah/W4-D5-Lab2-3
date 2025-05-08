d7223d713ddcb5db58ba3f0fcc49f4adf46a5fb6
Ayoosh <>
2025-05-08 12:16:03 +0300

    add lab3

--- lab3.js
+++ lab3.js
@@ -1,2 +1,28 @@
 // Try making the traffic light work - Think through the problem BEFORE you code!
 // You'll use addEventListener or Onclick and getElementById if you're doing it right...
+
+
+{/* <h1 id="stopButton" class="button">Stop</h1>
+<h1 id="slowButton" class="button">Slow</h1>
+<h1 id="goButton" class="button">Go</h1> */}
+
+
+let stopButton = document.getElementById("stopButton")
+let slowButton = document.getElementById("slowButton")
+let goButton = document.getElementById("goButton")
+let stopLight = document.getElementById("stopLight")
+let slowLight = document.getElementById("slowLight")
+let goLight = document.getElementById("goLight")
+
+
+
+stopButton.addEventListener("click", ()=>{
+    stopLight.style.backgroundColor= "red"
+})
+slowButton.addEventListener("click", ()=>{
+    slowLight.style.backgroundColor= "yellow"
+})
+goButton.addEventListener("click", ()=>{
+    goLight.style.backgroundColor= "green"
+})
+
