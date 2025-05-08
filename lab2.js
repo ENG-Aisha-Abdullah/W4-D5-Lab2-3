4a4f0ced89efcf68537c27c22f3046e8e9c57d84
Ayoosh <>
2025-05-08 11:45:32 +0300

    first commit

---
+++ lab2.js
@@ -0,0 +1,115 @@
+// When the user chooses one of the channel buttons, change the src attribute of the img inside the .tv-show div
+// You should write a single function to change the channel that accepts a single input, the channel number, and uses a conditional statement to change the TV to the image specific to that channel.
+// When you set up your event listeners for the buttons, the callback function should call upon that change channel function
+// Use a conditional statement to evaluate which channel they've chosen, so you can set a specific image for each channel
+// Feel free to look up and use any images of your choice and include them in your conditional statement!
+
+// Image files
+/*
+1-
+https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/blue-planet.jpg
+
+2-
+https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/deadpool.jpg
+
+3-
+https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/gravity.jpg
+
+4-
+https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/good-will-hunting.jpg
+
+5-
+https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/cnn.jpg
+
+6-
+https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/koyaanisqatsi.jpg
+
+7-
+https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/parts-unknown.jpg
+
+8-
+https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/star-wars-solo.jpeg
+
+9-
+https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/stranger-things.jpg
+*/
+
+// Variables
+// 1 - get channel from the remote
+// 2 - get the screen and change in the image in there
+
+// Pseudo-code
+// Variable1, listen to it for a click
+// function() {Listener would update the image in the screen}
+
+let one = document.getElementById("one")
+let two = document.getElementById("two")
+let three = document.getElementById("three")
+let four = document.getElementById("four")
+let five = document.getElementById("five")
+let six = document.getElementById("six")
+let seven = document.getElementById("seven")
+let eight = document.getElementById("eight")
+let nine = document.getElementById("nine")
+let img = document.getElementById("screen");
+
+function change(number) {
+    let img = document.getElementById("screen");
+    if (number === 1) {
+        img.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/blue-planet.jpg"
+    }
+    else if (number === 2) {
+        img.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/deadpool.jpg"
+    }
+    else if (number === 3) {
+        img.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/gravity.jpg"
+    }
+    else if (number === 4) {
+        img.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/good-will-hunting.jpg"
+    }
+    else if (number === 5) {
+        img.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/cnn.jpg"
+    }
+    else if (number === 6) {
+        img.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/koyaanisqatsi.jpg"
+    }
+    else if (number === 7) {
+        img.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/parts-unknown.jpg"
+    }
+    else if (number === 8) {
+        img.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/star-wars-solo.jpeg"
+    }
+    else if (number === 9) {
+        img.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/stranger-things.jpg"
+    }
+
+
+};
+
+one.addEventListener("click", () => {
+    change(1);
+});
+two.addEventListener("click", () => {
+    change(2)
+});
+three.addEventListener("click", () => {
+    change(3)
+});
+four.addEventListener("click", () => {
+    change(4)
+});
+five.addEventListener("click", () => {
+    change(5)
+});
+six.addEventListener("click", () => {
+    change(6)
+});
+seven.addEventListener("click", () => {
+    change(7)
+});
+eight.addEventListener("click", () => {
+    change(8)
+});
+nine.addEventListener("click", () => {
+    change(9)
+});
